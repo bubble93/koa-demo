583ae36d19075cfba26108d2a513eeffe24703dd
bubble <17791205330@163.com>
2019-10-22 21:28:18 +0800

    error

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+var fs = require('fs');
+var imgData = '';
 // 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
 
+
+
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
     await next();
+    fs.readFile('./111.png', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data);
+            imgData = data;
+            console.log(data.length + ' bytes');
+        }
+    });
     //设置允许跨域的域名，*代表允许任意域名跨域
     ctx.response.header("Access-Control-Allow-Origin","*");
     //允许的header类型
@@ -16,7 +29,7 @@ app.use(async (ctx, next) => {
     ctx.response.type = 'text/html';
     ctx.response.body = `
         {
-            src: '123'
+            src: ${imgData}
         }
     `;
 });
